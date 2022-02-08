6bcc457fbc2ead079dfcc1155247b3636f6329ce
rushi-kumari <rushi2rushionline@gmail.com>
2022-02-08 23:06:20 +0530

    js

---
+++ html/Js/index.js
@@ -0,0 +1,16 @@
+var a;
+a=3;
+console.log(a);
+console.log(typeof a);
+
+a="hello";
+console.log(a);
+console.log(typeof a);
+
+a="true";
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
