6b6463045c7830f97c7a896bc484a8ddc58bd63a
rushi-kumari <rushi2rushionline@gmail.com>
2022-02-08 23:23:32 +0530

    js data types

--- html/Js/index.js
+++ html/Js/index.js
@@ -14,3 +14,23 @@ console.log(typeof a);
 a = null;
 console.log(a);
 console.log(typeof a);
+
+
+//string
+// " ", ' ', ` `(backTicks) :  all are used for string in js
+var s = "hello";
+console.log(s);
+console.log(typeof s);
+
+//changing lines, both ways
+s = "hi guys.\nThis is Rushi."
+console.log(s);
+var b = `hi hope u doing well
+will talk to you later`;
+console.log(b);
+
+//making things dynamic
+var num = 100;
+console.log(`half of ${num} is ${num/2}`);
+//${}:makes things dynamic 
+//only works with backticks
